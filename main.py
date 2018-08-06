9c6e96685e397bdc3345261a0ac336f460c10b58
postBG <profile2697@gmail.com>
2018-08-07 01:47:14 +0900

    main scheme

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import argparse
+
+import torch
+
+from models import ConvNetWithGAP
+
+parser = argparse.ArgumentParser(description='PyTorch CAM')
+parser.add_argument('--lr', type=float, default=0.0001, help='learning rate (default: 0.0001)')
+parser.add_argument('--epoch', type=int, default=100, help='epoch (default: 100)')
+parser.add_argument('--output', type=str, default='output', help='dir to save model (default: output)')
+args = parser.parse_args()
+
+
+def main():
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = ConvNetWithGAP().to(device)
+
+
+if __name__ == "__main__":
+    main()
